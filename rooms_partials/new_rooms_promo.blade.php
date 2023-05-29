ebafab1021d01ca1e2e6f0217c62f3c112c0c5f3
CARLOS <gandalf930526@gmail.com>
2023-05-29 16:39:26 -0400

    arreglando promo rooms

--- rooms_partials/new_rooms_promo.blade.php
+++ rooms_partials/new_rooms_promo.blade.php
@@ -1,36 +1,36 @@
 <div class="flex flex-col justify-center items-center py-10 bg-gray-100 px-10">
-    <span class="text-gray-800 text-xl font-bold">
+    <span class="text-black text-xl font-bold">
         ¿Qué incluye tu habitación?
     </span>
     <div class="flex flex-row mt-8">
         <div class="flex flex-col justify-center items-center mt-4 px-4">
             <img class="w-12 h-12" src='https://www.helloflatmate.com/img/img/icon-mail.png'/>
-            <span class="text-gray-800 text-md font-bold mt-4">Contrato</span>
-            <p class="text-gray-700 text-sm mt-4 text-justify">
+            <span class="text-black text-md font-bold mt-4">Contrato</span>
+            <p class="text-gray-700 text-sm mt-4 text-justify px-6">
                 Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in error.
             </p>
         </div>
         <div class="flex flex-col justify-center items-center mt-4 px-4">
             <img class="w-12 h-12" src='https://www.helloflatmate.com/img/img/icon-card.png'/>
-            <span class="text-gray-800 text-md font-bold mt-4">Reserva inmediata</span>
-            <p class="text-gray-700 text-sm mt-4 text-justify">
+            <span class="text-black text-md font-bold mt-4">Reserva inmediata</span>
+            <p class="text-gray-700 text-sm mt-4 text-justify px-6">
                 Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in error.
             </p>
         </div>
         <div class="flex flex-col justify-center items-center mt-4 px-4">
             <img class="w-12 h-12" src='https://www.helloflatmate.com/img/img/icon-clock.png'/>
-            <span class="text-gray-800 text-md font-bold mt-4">Servicio 24/7</span>
-            <p class="text-gray-700 text-sm mt-4 text-justify">
+            <span class="text-black text-md font-bold mt-4">Servicio 24/7</span>
+            <p class="text-gray-700 text-sm mt-4 text-justify px-6">
                 Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in error.
             </p>
         </div>
         <div class="flex flex-col justify-center items-center mt-4 px-4">
             <img class="w-12 h-12" src='https://www.helloflatmate.com/img/img/icon-calendar.png'/>
-            <span class="text-gray-800 text-md font-bold mt-4">Check-in</span>
-            <p class="text-gray-700 text-sm mt-4 text-justify">
+            <span class="text-black text-md font-bold mt-4">Check-in</span>
+            <p class="text-gray-700 text-sm mt-4 text-justify px-6">
                 Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in error.
             </p>
