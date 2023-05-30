9506b074719f12888c61960e7e2aadb98f79ac35
CARLOS <gandalf930526@gmail.com>
2023-05-29 23:27:40 -0400

    arreglando grid rooms

--- rooms_partials/new_grid_rooms.blade.php
+++ rooms_partials/new_grid_rooms.blade.php
@@ -1,9 +1,13 @@
 <div x-data="cardsGrid()">
-    <div class="flex flex-wrap justify-center items-center px-14">
+    <div class="flex flex-wrap justify-center items-center px-20 mt-10">
         <template x-for="card in cards" :key="card">
-            <a href="#" class="flex flex-col items-center bg-gray-100 mt-4 ml-4 relative">
+            <a href="#" class="flex flex-col items-start bg-gray-100 mt-10 ml-10 relative">
                 <img class="object-cover w-full h-48" :src="card.imageSrc" alt="">
-                <span class="absolute bg-cyan-300 p-1 text-sm text-white font-bold" x-text="card.price"></span>
+                <span
+                      class="absolute bg-cyan-300 p-2 text-sm text-white font-bold bottom-1 right-1"
+                      x-text="card.price">
+                    e/mes
+                </span>
                 <div class="flex flex-col justify-between items-start p-1">
                     <h5 class="mb-2 text-sm font-bold text-black" x-text="card.title"></h5>
                     <p class="mb-2 text-sm text-gray-900" x-text="card.description"></p>
