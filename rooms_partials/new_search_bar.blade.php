c24488669ed9527e4aaee461f5c6aabe49ee69b3
CARLOS <gandalf930526@gmail.com>
2023-05-29 21:56:46 -0400

    arreglando search bar

--- rooms_partials/new_search_bar.blade.php
+++ rooms_partials/new_search_bar.blade.php
@@ -1,9 +1,9 @@
-<div class="bg-cyan-300 px-48 flex flex-row justify-between items-center">
+<div class="bg-cyan-300 px-48 flex flex-row justify-between items-center mb-4">
     <span class="text-white text-sm">Mostrando 9 de 50 resultados</span>
-    <div class="flex flex-row space-x-1">
-        <span class="text-black text-sm">¿Quiéres hacer una nueva búsqueda?</span>
+    <div class="flex flex-row items-center space-x-3">
+        <span class="text-black font-bold text-sm">¿Quiéres hacer una nueva búsqueda?</span>
         <input
-                class="placeholder-white bg-indigo-500 p-2"
+                class="placeholder-white text-white bg-violet-800 p-2"
                 placeholder="Buscar alojamiento"
         />
     </div>
