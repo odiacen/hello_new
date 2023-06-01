28c522e46e39e626dbd69aaf89bcae902440f45b
CARLOS <gandalf930526@gmail.com>
2023-06-01 00:08:50 -0400

    creando footer

--- global/new_footer.blade.php
+++ global/new_footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="footer flex-col justify-center items-center w-full bg-violet-800">
+<footer class="footer flex-col justify-center items-center w-full bg-violet-900">
     <div class="flex w-full justify-center flex-wrap text-left py-8 px-4">
         <div class="w-full lg:w-3/12 lg:px-4">
-            <div class="flex flex-wrap items-top">
-                <div class="w-full px-4 ml-auto flex flex-col">
-                    <span class="text-cyan-300 text-4xl mb-2 font-bold">hello</span>
-                    <span class="text-white text-4xl mb-2 font-bold">flatmate</span>
+            <div class="flex flex-wrap justify-center items-center">
+                <div class="w-full px-4 ml-auto flex flex-col ">
+                    <span class="text-cyan-300 text-6xl mb-2 font-bold">hello</span>
+                    <span class="text-white text-6xl mb-2 font-bold">flatmate</span>
                 </div>
             </div>
         </div>
@@ -12,9 +12,9 @@
             <div class="flex flex-wrap items-top">
                 <div class="w-full px-4 ml-auto flex flex-col">
                     <span class="text-white text-xl mb-2 font-bold">Contacto</span>
-                    <div class="flex flex-row">
-                      <span class="bg-white p-1 h-8 w-8 rounded-full flex justify-center items-center">
-                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-cyan-300">
+                    <div class="flex flex-row items-center">
+                      <span class="bg-white p-1 h-6 w-6 rounded-full flex justify-center items-center">
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-cyan-300">
                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                            </svg>
                       </span>
@@ -22,9 +22,9 @@
                            nfo@helloflatmate.com
                        </a>
                    </div>
-                    <div class="flex flex-row">
-                      <span class="bg-white p-1 h-8 w-8 rounded-full flex justify-center items-center">
-                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <div class="flex flex-row items-center">
+                      <span class="bg-white p-1 h-6 w-6 rounded-full flex justify-center items-center">
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-cyan-300">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                            </svg>
                       </span>
@@ -49,13 +49,13 @@
                     <a class="text-white text-sm">
                         Términos y condiciones
                     </a>
-                    <a class="text-white text-sm">
+                    <a class="text-white text-sm mt-2">
                         Política de cookies
                     </a>
-                    <a class="text-white text-sm">
+                    <a class="text-white text-sm mt-2">
                         Política de privacidad
                     </a>
-                    <a class="text-white text-sm">
+                    <a class="text-white text-sm mt-2">
                        Aviso legal
                     </a>
                 </div>
@@ -67,15 +67,18 @@
                     <a class="text-white font-bold text-md">
                         FAQs
                     </a>
-                    <a class="text-white font-bold text-md">
+                    <a class="text-white font-bold text-md mt-2">
                         Alojamientos
                     </a>
-                    <a class="text-white font-bold text-md">
+                    <a class="text-white font-bold text-md mt-2">
                         Contactos
                     </a>
-                    <a class="text-white font-bold text-md">
+                    <a class="text-white font-bold text-md mt-2">
                         Como trabajamos
                     </a>
+                    <span class="text-white font-bold text-md mt-8">
+                        Es/En
+                    </span>
                 </div>
             </div>
         </div>
@@ -85,13 +88,13 @@
         <span class="text-white text-sm">
             © 2023,
         </span>
-        <span class="text-white text-sm font-bold">
+        <span class="text-white text-sm font-bold ml-2">
             helloflatmate
         </span>
         <span class="text-white text-sm">
             . Todos los derechos reservados. Diseño web
         </span>
-        <span class="text-white text-sm font-bold">
+        <span class="text-white text-sm font-bold ml-2">
             Hello SEO&SEM.
         </span>
     </div>
