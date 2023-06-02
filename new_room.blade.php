6c2806e4433202fd189d7f163f44def4d3e4f921
CARLOS <gandalf930526@gmail.com>
2023-06-01 21:48:51 -0400

    creando carrusel

--- new_room.blade.php
+++ new_room.blade.php
@@ -1,1 +1,6 @@
-<h1>Habitación</h1>
+@include('nuevo.global.new_header')
+
+@include('nuevo.room_partials.new_carousel_room')
+
+@include('nuevo.global.new_faqs')
+@include('nuevo.global.new_footer')
