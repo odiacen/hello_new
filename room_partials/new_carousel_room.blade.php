1a73da21816217cda95a72176760f532b3cbb2f2
CARLOS <gandalf930526@gmail.com>
2023-06-01 23:58:04 -0400

    creando carrusel

--- room_partials/new_carousel_room.blade.php
+++ room_partials/new_carousel_room.blade.php
@@ -1,4 +1,4 @@
-<div x-data="carousel()" class="flex justify-center items-center h-screen">
+<div x-data="carousel()" x-init="startCarousel()" class="flex justify-center items-center h-screen">
     <div class="w-64">
         <div class="relative">
             <template x-for="(image, index) in images">
@@ -8,27 +8,43 @@
             </template>
 
             <div class="flex justify-between absolute w-full px-4">
-                <button @click="activeSlide = Math.max(activeSlide - 1, 0)" class="text-white text-2xl">&larr;</button>
-                <button @click="activeSlide = Math.min(activeSlide + 1, images.length - 1)" class="text-white text-2xl">&rarr;</button>
+                <button @click="activeSlide = Math.max(activeSlide - 1, 0)" class="text-cyan-300 bg-white text-xl p-1">
+                    <ion-icon name="chevron-back-outline"></ion-icon>
+                </button>
+                <button @click="activeSlide = Math.min(activeSlide + 1, images.length - 1)" class="text-cyan-300 bg-white text-xl p-1">
+                    <ion-icon name="chevron-forward-outline"></ion-icon>
+                </button>
             </div>
         </div>
     </div>
 
     <script>
         function carousel() {
+            const images = [
+                {
+                    imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
+                },
+                {
+                    imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
+                },
+                {
+                    imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
+                },
+            ];
+
+            let activeSlide = 0;
+            let intervalId = null;
+
+            function startCarousel() {
+                intervalId = setInterval(() => {
+                    activeSlide = (activeSlide + 1) % images.length;
+                }, 2000);
+            }
+
             return {
-                images: [
-                    {
-                        imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
-                    },
-                    {
-                        imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
-                    },
-                    {
-                        imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
-                    },
-                ],
-                activeSlide: 0,
+                images,
+                activeSlide,
+                startCarousel,
             };
         }
     </script>
