7126e9f0c55a39752c6eafdf4239442d58f06c9b
odiacen <96943520+odiacen@users.noreply.github.com>
2023-06-04 10:35:07 -0400

    load rooms

--- rooms_partials/new_grid_rooms.blade.php
+++ rooms_partials/new_grid_rooms.blade.php
@@ -1,21 +1,21 @@
-<div x-data="cardsGrid()" class="grid justify-center items-center">
+<div x-data="cardsGrid()" x-init="getRooms()" class="grid justify-center items-center">
     <div class="grid grid-cols-2 lg:grid-cols-3 justify-center items-center px-20 mt-2">
-        <template x-for="(card, index) in visibleCards" :key="card">
+        <template x-for="(room, index) in rooms">
             <a href="#" class="flex flex-col lg:w-[300px] w-[330px] items-start bg-gray-100 mt-10 ml-10 relative">
-                <img class="object-cover w-full lg:h-48 h-56" :src="card.imageSrc" alt="">
+                <img class="object-cover w-full lg:h-48 h-56" :src="portadas[index]" alt="">
                 <div class="absolute bg-cyan-300 flex flex-row p-2 text-sm text-white font-bold mb-1 bottom-14 right-1">
-                    <span x-text="card.price"></span>
+                    <span x-text="room.price"></span>
                     <span class="ml-2">e/mes</span>
                 </div>
                 <div class="flex flex-col justify-between items-start py-2 p-0.5 ml-4">
-                    <h5 class="text-sm font-bold text-black" x-text="card.title"></h5>
-                    <p class="text-sm text-gray-900" x-text="card.description"></p>
+                    <h5 class="text-sm font-bold text-black" x-text="room.title"></h5>
+                    <p class="text-sm text-gray-900" x-text="room.type"></p>
                 </div>
             </a>
         </template>
     </div>
     <div class="flex justify-center items-center">
-        <button x-show="showLoadMoreButton" @click="loadMoreCards"
+        <button x-show="showLoadMoreButton" @click="page++, getRooms()"
                 class="mt-10 bg-cyan-300 lg:w-1/4 w-3/4 hover:bg-cyan-400 text-white font-bold py-2 px-10 rounded-full">
             Cargar más
         </button>
@@ -23,128 +23,32 @@
 
     <script>
         function cardsGrid() {
-            const cards = [
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 275
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 375
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 285
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 275
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 375
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 285
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 275
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 375
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 285
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 275
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 375
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 285
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 275
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 375
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 285
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 275
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 375
-                },
-                {
-                    imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
-                    title: 'Tranquila habitación doble',
-                    description: 'En la mejor zona universitaria',
-                    price: 285
-                },
-            ];
-
-            const visibleCardsCount = 9;
-            const showLoadMoreButton = cards.length > visibleCardsCount;
-            let visibleCards = cards.slice(0, visibleCardsCount);
-
             return {
-                cards,
-                visibleCards,
-                showLoadMoreButton,
-                loadMoreCards() {
-                    const nextVisibleCardsCount = this.visibleCards.length + visibleCardsCount;
-                    this.visibleCards = cards.slice(0, nextVisibleCardsCount);
+                showLoadMoreButton: true,
+                endpointRooms: "https://www.helloflatmate.com/api/rooms?page=",
+                page: 1,
+                rooms: [],
+                portadas: [],
+                getRooms(){
+                    axios.get( this.endpointRooms + this.page )
+                    .then( (r)=>{
+                            
+                            if (r.data.rooms.data.length < 12) {
+                                this.showLoadMoreButton = false;
+                            }
+                            
+                            r.data.rooms.data.forEach(room => {
+                                this.rooms.push(room);
+                            });
+                            
+                            
+                            r.data.portadas.forEach(portada => {
+                                this.portadas.push(portada);
+                            });
+                                                                              
+                        }).catch( (e)=>{
+                            
+                        })
                 }
             };
         }
