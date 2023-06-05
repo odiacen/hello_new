2cb6eae78f43f24481f6a05cc0a5b8260f9f595d
odiacen <96943520+odiacen@users.noreply.github.com>
2023-06-05 17:23:19 -0400

    modal invalid search

--- home_partials/new_hero.blade.php
+++ home_partials/new_hero.blade.php
@@ -58,6 +58,23 @@
       Buscar
     </a>
   </form>
+
+  <div x-show="notify" id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-md max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            
+            <div class="p-6 text-center">
+                <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Debe seleccionar al menos un criterio de búsqueda para que sea válida. </h3>
+                <button @click="notify = false" data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                    Ok, Lo he comprendido.
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 </div>
       </div>
     </div>
@@ -78,11 +95,12 @@
                    zona: 'zona',
                    fecha: 'fecha',
                    univ: 'univ',
-                   redirect: false,
+                   notify: false,
                    searchUrl(){
                     
                     if(isNaN(this.tipo) && isNaN(this.zona) && isNaN(this.fecha) && isNaN(this.univ)){
                           console.log("Debe elegir al menos un criterio de búsqueda");
+                          this.notify = true;
                         }else{
                           console.log(this.tipo, this.zona, this.fecha, this.univ);
                         }    
