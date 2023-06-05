59c2ef1dddfc312d19d1bd70b8028accd7d9592e
odiacen <96943520+odiacen@users.noreply.github.com>
2023-06-05 14:38:29 -0400

    search button

--- home_partials/new_hero.blade.php
+++ home_partials/new_hero.blade.php
@@ -18,14 +18,14 @@
   <form>
     <div class="grid grid-cols-4 gap-4">
       <!--First name input-->
-      <select class="peer block w-full h-8 rounded border-0 bg-white" data-te-select-init data-te-select-visible-options="3">
+      <select x-model="tipo" class="peer block w-full h-8 rounded border-0 bg-white" data-te-select-init data-te-select-visible-options="3">
           <option value="1">Tipo de alojamiento</option>
-          <option value="3">Habitaciones</option>
-          <option value="4">Coliving</option>
+          <option value="2">Habitaciones</option>
+          <option value="3">Coliving</option>
        </select>
      
       <!--First name input-->
-      <select class="peer block w-full h-8 rounded border-0 bg-white" data-te-select-init data-te-select-visible-options="3">
+      <select x-model="zona" class="peer block w-full h-8 rounded border-0 bg-white" data-te-select-init data-te-select-visible-options="3">
           <option value="1">¿En que zona?</option>
           <template x-for="zone in zones">
               <option x-text="zone.zone"></option>
@@ -33,7 +33,7 @@
        </select>
        
        <!--First name input-->
-      <select class="peer block w-full h-8 rounded border-0 bg-white" data-te-select-init data-te-select-visible-options="3">
+      <select x-model="fecha" class="peer block w-full h-8 rounded border-0 bg-white" data-te-select-init data-te-select-visible-options="3">
           <option value="1">¿Para qué fecha?</option>
           <template x-for="fecha in fechas">
               <option x-text="fecha.name"></option>
@@ -41,7 +41,7 @@
        </select>
        
        <!--First name input-->
-      <select class="peer block w-full h-8 rounded border-0 bg-white" data-te-select-init data-te-select-visible-options="3">
+      <select x-model="univ" class="peer block w-full h-8 rounded border-0 bg-white" data-te-select-init data-te-select-visible-options="3">
           <option value="1">Cerca de tu universidad</option>
           <template x-for="univ in univs">
               <option x-text="univ.university"></option>
@@ -50,12 +50,13 @@
 
       
     <!--Submit button-->
-    <button
+    <a
+      @click="searchUrl()"
       type="submit"
       class="inline-block md:w-[60%] rounded-full bg-cyan-300 px-6 pb-2 pt-2.5 text-xs font-bold uppercase leading-normal text-stone-950 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
       >
       Buscar
-    </button>
+    </a>
   </form>
 </div>
       </div>
@@ -73,6 +74,14 @@
                    zones: [],
                    fechas: [],
                    univs: [],
+                   tipo: 'tipo',
+                   zona: 'zona',
+                   fecha: 'fecha',
+                   univ: 'univ',
+                   redirect: false,
+                   searchUrl(){
+                        console.log(this.tipo, this.zona, this.fecha, this.univ);
+                   }, 
                    axiosCall(endpoint){
                         axios.get( endpoint )
                       .then( (r)=>{
--- new_search.blade.php
+++ new_search.blade.php
@@ -3,6 +3,5 @@
 @include('nuevo.rooms_partials.new_grid_rooms')
 @include('nuevo.search_partials.new_alpine_rooms')
 </section>
-@include('nuevo.rooms_partials.new_faqs_rooms')
 @include('nuevo.global.new_faqs')
 @include('nuevo.global.new_footer')
