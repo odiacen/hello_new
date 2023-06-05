ed65201120166c6b5592145727f15beb8bc62c51
odiacen <96943520+odiacen@users.noreply.github.com>
2023-06-05 05:55:23 -0400

    home faq and footer

--- new_home.blade.php
+++ new_home.blade.php
@@ -11,3 +11,6 @@
 <section>
     @include('nuevo.home_partials.new_carrusell')
 </section>
+@include('nuevo.rooms_partials.new_faqs_rooms')
+@include('nuevo.global.new_faqs')
+@include('nuevo.global.new_footer')
