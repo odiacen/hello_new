42b33a454dd590f43c0a920ae1c0b48684b2e2ea
odiacen <96943520+odiacen@users.noreply.github.com>
2023-06-05 17:29:14 -0400

    update modal search notification

--- home_partials/new_hero.blade.php
+++ home_partials/new_hero.blade.php
@@ -59,7 +59,7 @@
     </a>
   </form>
 
-  <div x-show="notify" id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+  <div :class="notify ? `` : `hidden`" id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-md max-h-full">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             
