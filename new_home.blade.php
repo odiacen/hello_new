edbb9a7fa4a6a3c66935a3cd65462ae73519b0a3
odiacen <96943520+odiacen@users.noreply.github.com>
2023-06-08 10:22:32 -0400

    UPDATE HOME

--- new_home.blade.php
+++ new_home.blade.php
@@ -2,8 +2,7 @@
 
 <section>@include('nuevo.home_partials.new_hero')</section>
 
-@include('nuevo.home_partials.new_promo')
-@include('nuevo.home_partials.new_carruselr')
+
 
 @include('nuevo.global.new_faqs')
 @include('nuevo.global.new_footer')
