74b6def20465eaca9abfb31f25671581972a5484
CARLOS <gandalf930526@gmail.com>
2023-06-09 20:56:35 -0400

    creando carrusel

--- home_partials/new_carruselr.blade.php
+++ home_partials/new_carruselr.blade.php
@@ -2,44 +2,34 @@
   activeTab: 'tab1',
   tabs: [
     {
-        id: 'tab1',
-        icon: 'https://www.helloflatmate.com/img/img/icon-mail.png',
-        label: 'Contrato'
-        content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir.
-                Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al
-                Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
+      id: 'tab1',
+      icon: 'https://www.helloflatmate.com/img/img/icon-mail.png',
+      label: 'Contrato',
+      content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir. Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
     },
     {
-        id: 'tab2',
-        icon: 'https://www.helloflatmate.com/img/img/icon-clock.png',
-        label: 'Emergencias 24/7'
-        content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir.
-                Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al
-                Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
+      id: 'tab2',
+      icon: 'https://www.helloflatmate.com/img/img/icon-clock.png',
+      label: 'Emergencias 24/7',
+      content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir. Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
     },
     {
-        id: 'tab3',
-        icon: 'https://www.helloflatmate.com/img/img/icon-setting.png',
-        label: 'Mantenimiento'
-        content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir.
-                Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al
-                Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
+      id: 'tab3',
+      icon: 'https://www.helloflatmate.com/img/img/icon-setting.png',
+      label: 'Mantenimiento',
+      content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir. Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
     },
     {
-        id: 'tab4',
-        icon: 'https://www.helloflatmate.com/img/img/icon-shower.png',
-        label: 'Suministros'
-        content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir.
-                Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al
-                Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
+      id: 'tab4',
+      icon: 'https://www.helloflatmate.com/img/img/icon-shower.png',
+      label: 'Suministros',
+      content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir. Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
     },
     {
-        id: 'tab5',
-        icon: 'https://www.helloflatmate.com/img/img/icon-person-check.png',
-        label: 'Medicación entre inquilinos'
-        content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir.
-                Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al
-                Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
+      id: 'tab5',
+      icon: 'https://www.helloflatmate.com/img/img/icon-person-check.png',
+      label: 'Medicación entre inquilinos',
+      content: 'Es muy importante que como inquilino tenga contrato de alquiler y así tener un alojamiento legal donde residir. Por ello todos los alojamientos que gestionamos tienen el mismo modelo de contrato temporal y que se acoge al Código Civil y que equipara en derecho y obligación tanto a arrendador como arrendatario.'
     }
   ]
 }">
@@ -51,16 +41,14 @@
                     <span class="text-black font-bold text-4xl">¿Por qué es mucho más</span>
                     <span class="text-cyan-300 font-bold text-4xl">que una habitación?</span>
                 </div>
-               <div class="px-8">
-                    <span class="text-white font-bold text-2xl">
-                        Habitaciones - hello rooms
-                    </span>
-                   <p class="text-md text-justify text-black">
-                       Las habitaciones <strong>HELLO ROOMS</strong>, son habitaciones que prepara cada año helloflatmate
-                       encargándose de la limpieza, mantenimiento y gestión de la estancia de flatmates.
-                       Para que el tiempo que esté con nosotros no se tenga que preocupar prácticamente de nada.
-                   </p>
-               </div>
+                <div class="px-8 mt-4">
+                  <span class="text-white font-bold text-2xl">
+                    Habitaciones - hello rooms
+                  </span>
+                    <p class="text-md text-justify text-black">
+                        Las habitaciones <strong>HELLO ROOMS</strong>, son habitaciones que prepara cada año helloflatmate encargándose de la limpieza, mantenimiento y gestión de la estancia de flatmates. Para que el tiempo que esté con nosotros no se tenga que preocupar prácticamente de nada.
+                    </p>
+                </div>
             </div>
             <hr class="border-t-2 border-gray-50 my-2">
             <template x-for="tab in tabs" :key="tab.id">
@@ -77,11 +65,12 @@
                     class="flex flex-row items-center justify-center font-bold rounded-full p-2"
                     :class="{ 'bg-cyan-300 text-white': activeTab === tab.id, 'bg-white text-black': activeTab !== tab.id }"
                 >
-                    <span
-                        class="rounded-full h-8 w-8 p-2"
-                        :class="{ 'bg-white': activeTab === tab.id, 'bg-gray-100': activeTab !== tab.id }">
-                       <img class="h-6 w-6" :src="tab.icon" />
-                    </span>
+                  <span
+                          class="rounded-full h-8 w-8 p-2"
+                          :class="{ 'bg-white': activeTab === tab.id, 'bg-gray-100': activeTab !== tab.id }"
+                  >
+                     <img class="h-6 w-6" :src="tab.icon" />
+                  </span>
                     <span x-text="tab.label"></span>
                 </button>
             </template>
