fa3fe26c717434536cfa032609b18b129a2c8573
CARLOS <gandalf930526@gmail.com>
2023-06-09 23:50:41 -0400

    creando carrusel

--- new_home.blade.php
+++ new_home.blade.php
@@ -3,6 +3,7 @@
 <section>@include('nuevo.home_partials.new_hero')</section>
 @include('nuevo.home_partials.new_promo')
 @include('nuevo.home_partials.new_carruselr')
+@include('nuevo.home_partials.new_carrusell')
 
 
 @include('nuevo.global.new_faqs')
