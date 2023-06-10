267025e60fca83f6a168f962294e159a03cc8dd6
CARLOS <gandalf930526@gmail.com>
2023-06-10 14:40:24 -0400

    creando arrival

--- home_partials/new_arrival.blade.php
+++ home_partials/new_arrival.blade.php
@@ -1,9 +1,9 @@
-<div class="flex flex-row justify-center items-center h-[80%] h-[30%]">
-    <div class="flex flex-col justify-center items-center w-1/3 px-8 ml-10">
+<div class="flex flex-row justify-center items-center lg:h-[80%] h-[30%]">
+    <div class="flex flex-col justify-center items-center w-1/3 px-8 ml-20">
         <span class="text-cyan-300 font-bold text-2xl">La llegada</span>
         <span class="text-black text-md text-justify">Nunca ha sido tan fácil llegar a la ciudad de Valencia, reserva con nosotros y nos encargamos del resto.</span>
     </div>
-    <div class="h-[80%] h-[30%] w-2/3" style="
+    <div class="lg:h-[80%] h-[30%] w-2/3" style="
         background-position: 50%;
         background-image: url('https://www.helloflatmate.com/img/img/4.png');
         -webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 40% 50%);
