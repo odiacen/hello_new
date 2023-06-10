24edc4da1e5935723dc9dfdb2ab86af2f7fdffc2
CARLOS <gandalf930526@gmail.com>
2023-06-10 11:39:45 -0400

    creando arrival

--- home_partials/new_arrival.blade.php
+++ home_partials/new_arrival.blade.php
@@ -1,12 +1,13 @@
-<div class="flex flex-row justify-center items-center h-[60%] h-[40%]">
-    <div class="flex flex-col justify-center items-center w-1/3">
+<div class="flex flex-row justify-center items-center h-[80%] h-[40%]">
+    <div class="flex flex-col justify-center items-center w-1/3 px-8">
         <span class="text-cyan-300 font-bold text-2xl">La llegada</span>
-        <span class="text-black text-md">Nunca ha sido tan fácil llegar a la ciudad de Valencia, reserva con nosotros y nos encargamos del resto.</span>
+        <span class="text-black text-md text-justify">Nunca ha sido tan fácil llegar a la ciudad de Valencia, reserva con nosotros y nos encargamos del resto.</span>
     </div>
     <div class="h-[60%] h-[40%] w-2/3" style="
       background-position: 50%;
       background-image: url('https://www.helloflatmate.com/img/img/4.png');
-      /*clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%, 0% 30%);*/
+      -webkit-clip-path: polygon(0 0, 100% 0, 0% 100%);
+      clip-path: polygon(0 0, 100% 0, 0% 100%);
     ">
     </div>
 </div>
--- home_partials/new_carruselr.blade.php
+++ home_partials/new_carruselr.blade.php
@@ -46,7 +46,7 @@
                       <span class="text-white font-bold lg:text-2xl text-xl">
                         Habitaciones - hello rooms
                       </span>
-                        <p class="lg:text-md text-sd text-justify text-black">
+                        <p class="lg:text-md text-sd text-justify text-black lg:h-auto h-[40%] overflow-y-auto">
                             Las habitaciones <strong>HELLO ROOMS</strong>, son habitaciones que prepara cada año helloflatmate encargándose de la limpieza, mantenimiento y gestión de la estancia de flatmates. Para que el tiempo que esté con nosotros no se tenga que preocupar prácticamente de nada.
                         </p>
                     </div>
@@ -56,7 +56,7 @@
                     <template x-for="tab in tabs" :key="tab.id">
                         <div x-show="activeTab === tab.id">
                             <span class="text-black font-bold lg:text-2xl text-xl" x-text="tab.label"></span>
-                            <p class="text-black text-justify lg:text-md text-sd" x-text="tab.content"></p>
+                            <p class="text-black text-justify lg:text-md text-sd lg:h-auto h-[40%] overflow-y-auto" x-text="tab.content"></p>
                         </div>
                     </template>
                 </div>
