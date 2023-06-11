689aaa8c7340aba86efb5be0ce28537b58f7b0c5
CARLOS <gandalf930526@gmail.com>
2023-06-10 23:37:54 -0400

    creando medio ambiente

--- home_partials/new_environment.blade.php
+++ home_partials/new_environment.blade.php
@@ -1,10 +1,10 @@
 <div class="flex flex-row w-full mt-14">
     <img class="w-1/2" src="https://www.helloflatmate.com/img/img/image1.png">
     <div class="w-1/2 flex flex-col">
-        <div class="h-[40%] bg-cyan-300 lg:px-20 px-16 lg:pr-10 text-white text-4xl font-bold flex justify-center items-center">
-            ¡Compromiso con el medio ambiente!
+        <div class="h-[40%] bg-cyan-300 lg:px-20 px-16 flex justify-center items-center">
+           <span class="text-white text-4xl font-bold lg:pr-10"> ¡Compromiso con el medio ambiente!</span>
         </div>
-        <div class="h-[60%] flex flex-col justify-start items-start py-10 relative lg:px-20 px-10 space-y-10">
+        <div class="h-[60%] flex flex-col justify-start items-start py-10 relative lg:px-20 px-10 space-y-4 lg:space-y-10">
             <span class="text-black text-2xl font-bold">Reforestar +250 árboles</span>
             <p class="text-black text-justify text-sm pr-16">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in error.</p>
             <button class="flex justify-center items-center bg-black text-white font-bold text-xl p-2 rounded-full">
