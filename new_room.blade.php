467072dd0d65ea4a34e6c2b2a1662e66811acc24
CARLOS <gandalf930526@gmail.com>
2023-06-11 15:44:41 -0400

    faqs room

--- new_room.blade.php
+++ new_room.blade.php
@@ -1,6 +1,7 @@
 @include('nuevo.global.new_header')
 
 @include('nuevo.room_partials.new_carousel_room')
+@include('nuevo.room_partials.new_faqs_room')
 
 @include('nuevo.global.new_faqs')
 @include('nuevo.global.new_footer')
