0da553bfe0b315dfa0cf66b6e358e0cff5613025
CARLOS <gandalf930526@gmail.com>
2023-06-11 11:35:27 -0400

    room

--- room_partials/new_carousel_room.blade.php
+++ room_partials/new_carousel_room.blade.php
@@ -1,10 +1,10 @@
-<div x-data="carousel()" x-init="startCarousel()" class="flex flex-col justify-start items-start mt-20 px-36">
-    <div class="flex flex-row w-full">
+<div x-data="carousel()" x-init="startCarousel()" class="flex flex-col justify-center items-start mt-20 px-36">
+    <div class="flex flex-row justify-between w-full">
         <div class="flex flex-col">
             <span class="text-black text-xl font-bold">Tranquila habitación doble</span>
             <span class="text-black text-sm">Zona centro</span>
         </div>
-        <span class="bg-neutral-200 text-black p-1">
+        <span class="bg-neutral-200 text-black text-sm p-1">
             Cod. habitación <strong>ZNANSDHAJAKJA</strong>
         </span>
     </div>
@@ -29,24 +29,60 @@
         </div>
         <div class="flex flex-col justify-start items-start ml-10 space-y-4">
             <span class="bg-cyan-300 text-white font-bold text-sm p-2 w-1/2">275 e/mes</span>
-            <button class="bg-[#3f12a3] flex flex-row justify-center items-center p-1 rounded-full text-white font-bold">
+            <button class="bg-[#3f12a3] flex flex-row justify-center items-center p-1 rounded-full text-white text-sm font-bold">
                 Reserva sin esperas
-                <span class="bg-white rounded-full p-2 h-6 w-6 flex justify-center items-center ml-2">
+                <span class="bg-white rounded-full p-1 h-6 w-6 flex justify-center items-center ml-2">
                     <ion-icon class="text-black h-4 w-4" name="add-outline"></ion-icon>
                 </span>
             </button>
-            <button class="bg-gray-500 flex flex-row justify-center items-center p-1 rounded-full text-white font-bold">
+            <button class="bg-gray-500 flex flex-row justify-center items-center p-1 rounded-full text-white text-sm font-bold">
                 ¿Alguna pregunta?
-                <span class="bg-white rounded-full p-2 h-6 w-6 flex justify-center items-center ml-2">
+                <span class="bg-white rounded-full p-1 h-6 w-6 flex justify-center items-center ml-2">
                     <ion-icon class="text-black h-4 w-4" name="add-outline"></ion-icon>
                 </span>
             </button>
-            <button class="bg-cyan-300 flex flex-row justify-center items-center p-1 rounded-full text-white font-bold">
+            <button class="bg-cyan-300 flex flex-row justify-center items-center p-1 rounded-full text-white text-sm font-bold">
                 Como reservar
-                <span class="bg-white rounded-full p-2 h-6 w-6 flex justify-center items-center ml-2">
+                <span class="bg-white rounded-full p-1 h-6 w-6 flex justify-center items-center ml-2">
                     <ion-icon class="text-black h-4 w-4" name="add-outline"></ion-icon>
                 </span>
             </button>
         </div>
     </div>
+
+    <script>
+        function carousel() {
+            const images = [
+                {
+                    imageSrc: 'https://www.helloflatmate.com/img/img/room1.png',
+                },
+                {
+                    imageSrc: 'https://www.helloflatmate.com/img/img/room2.png',
+                },
+                {
+                    imageSrc: 'https://www.helloflatmate.com/img/img/room3.png',
+                },
+            ];
+
+            return {
+                images,
+                activeSlide: 0,
+                startCarousel() {
+                    setInterval(() => {
+                        this.nextSlide();
+                    }, 10000);
+                },
+                isActiveSlide(index) {
+                    return this.activeSlide === index;
+                },
+                previousSlide() {
+                    this.activeSlide = (this.activeSlide - 1 + this.images.length) % this.images.length;
+                },
+                nextSlide() {
+                    this.activeSlide = (this.activeSlide + 1) % this.images.length;
+                },
+            };
+        }
+    </script>
 </div>
+
