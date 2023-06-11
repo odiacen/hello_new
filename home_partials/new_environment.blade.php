add54cbea4f89cace0478ac6cbdeea0398650110
CARLOS <gandalf930526@gmail.com>
2023-06-11 09:33:49 -0400

    creando medios de noticias

--- home_partials/new_environment.blade.php
+++ home_partials/new_environment.blade.php
@@ -1,16 +1,16 @@
 <div class="flex flex-row w-full mt-14">
     <img class="w-1/2" src="https://www.helloflatmate.com/img/img/image1.png">
     <div class="w-1/2 flex flex-col">
-        <div class="h-[40%] bg-cyan-300 lg:px-20 px-16 flex justify-center items-center">
+        <div class="h-[40%] bg-cyan-300 lg:px-20 px-16 flex justify-start items-center">
            <span class="text-white text-4xl font-bold lg:w-1/2"> ¡Compromiso con el medio ambiente!</span>
         </div>
-        <div class="h-[60%] flex flex-col justify-start items-start lg:py-10 py-4 relative lg:px-20 px-6 space-y-4 lg:space-y-4">
+        <div class="h-[60%] flex flex-col justify-start items-start lg:py-10 py-4 relative lg:px-20 px-6 space-y-4 lg:space-y-10">
             <span class="text-black text-2xl font-bold">Reforestar +250 árboles</span>
             <p class="text-black text-justify text-sm pr-16">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in error.</p>
             <button class="flex justify-center items-center bg-black text-white font-bold text-xl p-2 rounded-full">
                 Quiero saber más
             </button>
-            <div class="bg-[#e7c007] text-black font-bold lg:text-xl text-md flex justify-center items-center absolute lg:-bottom-10 p-2 px-8 w-1/2 h-[120px] lg:right-20 right-10 bottom-4">
+            <div class="bg-[#e7c007] text-black font-bold lg:text-xl text-md flex justify-center items-center absolute lg:-bottom-10 p-2 px-8 w-1/2 h-[120px] lg:right-20 right-4 -bottom-4">
                 Aquí deben ir las últimas noticias o bien un enlace a todas
             </div>
         </div>
