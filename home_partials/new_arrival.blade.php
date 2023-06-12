721c4bdd403076f06805cf38d25a877e0575bbc0
CARLOS <gandalf930526@gmail.com>
2023-06-12 12:20:42 -0400

    arreglando arrival

--- home_partials/new_arrival.blade.php
+++ home_partials/new_arrival.blade.php
@@ -5,33 +5,28 @@
     </div>
     <div class="h-full lg:w-3/4 w-[85%] relative flex justify-center items-center" style="
         background-position: 50%;
-        background-image: url('https://youtu.be/3KSXxo8CbpY');
+        background-image: url('https://www.helloflatmate.com/img/img/4.png');
         -webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 30% 50%);
         clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 30% 50%);
     ">
-        <img class="w-20 h-auto -ml-12" src="https://www.helloflatmate.com/img/img/point-map.png">
-        <span onclick="playVideo()" class="w-14 h-14 bg-white rounded-full p-1 flex justify-center items-center ml-12">
-            <img class="w-12 h-12" src="https://www.helloflatmate.com/img/img/icon-play.png">
-        </span>
-        <div class="flex flex-col justify-start absolute top-20 right-20">
-            <span class="text-white text-sm font-bold">BIENVENIDOS</span>
-            <span class="text-cyan-300 text-sm font-bold">A HELLO</span>
+        <div class="video-container">
+            <img class="w-20 h-auto -ml-12" src="https://www.helloflatmate.com/img/img/point-map.png">
+            <span class="w-14 h-14 bg-white rounded-full p-1 flex justify-center items-center ml-12" onclick="playVideo()">
+                <img class="w-12 h-12" src="https://www.helloflatmate.com/img/img/icon-play.png">
+            </span>
+            <div class="flex flex-col justify-start absolute top-20 right-20">
+                <span class="text-white text-sm font-bold">BIENVENIDOS</span>
+                <span class="text-cyan-300 text-sm font-bold">A HELLO</span>
+            </div>
         </div>
     </div>
 </div>
 <script>
     function playVideo() {
-        var videoUrl = 'https://youtu.be/3KSXxo8CbpY';
+        var videoUrl = 'https://www.youtube.com/watch?v=nyxTSCVz8w8';
         var embedUrl = videoUrl.replace('watch?v=', 'embed/');
-        var videoPreview = document.querySelector('.h-full');
-        var videoPlayer = document.createElement('iframe');
+        var videoContainer = document.querySelector('.video-container');
 
-        videoPlayer.setAttribute('src', embedUrl);
-        videoPlayer.setAttribute('allowfullscreen', '');
-        videoPlayer.style.width = '100%';
-        videoPlayer.style.height = '100%';
-
-        videoPreview.innerHTML = '';
-        videoPreview.appendChild(videoPlayer);
+        videoContainer.innerHTML = '<iframe width="100%" height="100%" src="' + embedUrl + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
 </script>
