3995e48b9bd0e1415079386393fce0842393acea
CARLOS <gandalf930526@gmail.com>
2023-06-12 11:34:41 -0400

    arreglando arrival

--- home_partials/new_arrival.blade.php
+++ home_partials/new_arrival.blade.php
@@ -3,19 +3,23 @@
         <span class="text-cyan-300 font-bold text-2xl">La llegada</span>
         <span class="text-black text-sm text-justify">Nunca ha sido tan fácil llegar a la ciudad de Valencia, reserva con nosotros y nos encargamos del resto.</span>
     </div>
-    <div class="h-full lg:w-3/4 w-[85%] relative flex justify-center items-center" style="
-        background-position: 50%;
-        background-image: url('https://www.helloflatmate.com/img/img/4.png');
-        -webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 30% 50%);
-        clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 30% 50%);
-    ">
-        <img class="w-20 h-auto -ml-12" src="https://www.helloflatmate.com/img/img/point-map.png">
-        <span class="w-14 h-14 bg-white rounded-full p-1 flex justify-center items-center ml-12">
+    <div class="h-full lg:w-3/4 w-[85%] relative flex justify-center items-center" style="background-position: 50%; background-image: url('https://www.helloflatmate.com/img/img/4.png'); -webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 30% 50%); clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 30% 50%);">
+        <video id="videoPlayer" src="https://vimeo.com/28281304" loop style="width: 100%; height: 100%; object-fit: cover;"></video>
+        <div class="play-button" onclick="playVideo()">
             <img class="w-12 h-12" src="https://www.helloflatmate.com/img/img/icon-play.png">
-        </span>
+        </div>
         <div class="flex flex-col justify-start absolute top-20 right-20">
             <span class="text-white text-sm font-bold">BIENVENIDOS</span>
             <span class="text-cyan-300 text-sm font-bold">A HELLO</span>
         </div>
     </div>
 </div>
+
+<script>
+    function playVideo() {
+        var video = document.getElementById('videoPlayer');
+        var playButton = document.querySelector('.play-button');
+        video.play();
+        playButton.style.display = 'none';
+    }
+</script>
