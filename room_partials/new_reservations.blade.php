86759e37f5e3021a4375c75a103e79642c533b3e
CARLOS <gandalf930526@gmail.com>
2023-06-12 11:07:03 -0400

    time line

--- room_partials/new_reservations.blade.php
+++ room_partials/new_reservations.blade.php
@@ -1,33 +1,33 @@
-<div class="flex flex-col justify-center items-center mt-24 lg:px-36 px-10">
+<div class="flex flex-col justify-center items-center mt-24 lg:px-36 px-16">
     <span class="text-2xl text-black font-bold">Cómo reservar</span>
     <ol class="relative border-l-2 border-gray-200">
         <li class="ml-10">
-        <span class="absolute flex items-center justify-center w-16 h-16 bg-[#91dfe4] rounded-full -left-10 text-white">
+        <span class="absolute flex items-center justify-center w-16 h-16 bg-[#91dfe4] rounded-full -left-8 text-white">
             <ion-icon class="w-10 h-10" name="calendar-outline"></ion-icon>
         </span>
-            <h3 class="flex items-center mb-1 text-lg font-bold text-gray-900">Comprueba fechas</h3>
-            <p class="mb-4 text-base font-normal text-gray-500">Comprueba que la fecha seleccionada es la correcta y haz click en reservar. Nota: Si la duración de tu estancia es diferente a la oferta contacta con rooms@helloflatmate.com</p>
+            <h3 class="flex items-center mb-1 text-lg font-bold text-gray-900 ml-4">Comprueba fechas</h3>
+            <p class="mb-4 text-base font-normal text-gray-500 ml-4">Comprueba que la fecha seleccionada es la correcta y haz click en reservar. Nota: Si la duración de tu estancia es diferente a la oferta contacta con rooms@helloflatmate.com</p>
         </li>
         <li class="ml-10 mt-8">
-        <span class="absolute flex items-center justify-center w-16 h-16 bg-[#68a3dd] rounded-full -left-10 text-white">
+        <span class="absolute flex items-center justify-center w-16 h-16 bg-[#68a3dd] rounded-full -left-8 text-white">
             <ion-icon class="w-10 h-10" name="document-text-outline"></ion-icon>
         </span>
-            <h3 class="mb-1 text-lg font-semibold text-gray-900">Cumplimenta</h3>
-            <p class="text-base font-normal text-gray-500">Introduce los datos personales que indica la pasarela de pago y haz click.</p>
+            <h3 class="mb-1 text-lg font-semibold text-gray-900 ml-4">Cumplimenta</h3>
+            <p class="text-base font-normal text-gray-500 ml-4">Introduce los datos personales que indica la pasarela de pago y haz click.</p>
         </li>
         <li class="ml-10 mt-8">
-        <span class="absolute flex items-center justify-center w-16 h-16 bg-[#4f68c6] rounded-full -left-10 text-white">
+        <span class="absolute flex items-center justify-center w-16 h-16 bg-[#4f68c6] rounded-full -left-8 text-white">
             <img class="w-9 h-9" src="https://www.helloflatmate.com/img/img/icon-calendar-check.png">
         </span>
-            <h3 class="mb-1 text-lg font-semibold text-gray-900">Reserva sin esperas</h3>
-            <p class="text-base font-normal text-gray-500">Trabajamos con el Cyberpac más seguro del mercado español (La Caixa) Cumplimenta los datos que te pide, una vez hecho y realizado el pago recibirás un correo electrónico de confirmación. ¡Y ya está!, habitación reservada sin esperas de confirmación.</p>
+            <h3 class="mb-1 text-lg font-semibold text-gray-900 ml-4">Reserva sin esperas</h3>
+            <p class="text-base font-normal text-gray-500 ml-4">Trabajamos con el Cyberpac más seguro del mercado español (La Caixa) Cumplimenta los datos que te pide, una vez hecho y realizado el pago recibirás un correo electrónico de confirmación. ¡Y ya está!, habitación reservada sin esperas de confirmación.</p>
         </li>
         <li class="ml-10 mt-8">
-        <span class="absolute flex items-center justify-center w-16 h-16 bg-[#3f16a5] rounded-full -left-10 text-white">
+        <span class="absolute flex items-center justify-center w-16 h-16 bg-[#3f16a5] rounded-full -left-8 text-white">
             <ion-icon class="w-10 h-10" name="mail-outline"></ion-icon>
         </span>
-            <h3 class="mb-1 text-lg font-semibold text-gray-900 ">Confirmación</h3>
-            <p class="text-base font-normal text-gray-500">En breve recibirás un correo electrónico para organizar tu llegada a Valencia. Y así tener tu contrato y llaves preparadas. ¡Bienvenida/o a Valencia! ¡Bienvenida/o a helloflatmate!</p>
+            <h3 class="mb-1 text-lg font-semibold text-gray-900 ml-4">Confirmación</h3>
+            <p class="text-base font-normal text-gray-500 ml-4">En breve recibirás un correo electrónico para organizar tu llegada a Valencia. Y así tener tu contrato y llaves preparadas. ¡Bienvenida/o a Valencia! ¡Bienvenida/o a helloflatmate!</p>
         </li>
     </ol>
 </div>
