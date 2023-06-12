3ead5503c68ae19aeff6bf95b29f658acb4ebc3b
CARLOS <gandalf930526@gmail.com>
2023-06-12 10:44:42 -0400

    time line

--- room_partials/new_reservations.blade.php
+++ room_partials/new_reservations.blade.php
@@ -1,31 +1,31 @@
 
-<div class="flex flex-col justify-center items-center">
+<div class="flex flex-col justify-center items-center mt-24 px-36">
     <span class="text-2xl text-black font-bold">Cómo reservar</span>
     <ol class="relative border-l border-gray-200">
         <li class="ml-6">
-        <span class="absolute flex items-center justify-center w-8 h-8 bg-[#91dfe4] rounded-full -left-3 ring-8 ring-white text-white">
-            <ion-icon clase="w-5 h-5" name="calendar-outline"></ion-icon>
+        <span class="absolute flex items-center justify-center w-14 h-14 bg-[#91dfe4] rounded-full -left-3 ring-8 ring-white text-white">
+            <ion-icon class="w-8 h-8" name="calendar-outline"></ion-icon>
         </span>
             <h3 class="flex items-center mb-1 text-lg font-bold text-gray-900">Comprueba fechas</h3>
             <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Comprueba que la fecha seleccionada es la correcta y haz click en reservar. Nota: Si la duración de tu estancia es diferente a la oferta contacta con rooms@helloflatmate.com</p>
         </li>
         <li class="ml-6">
-        <span class="absolute flex items-center justify-center w-8 h-8 bg-[#68a3dd] rounded-full -left-3 ring-8 ring-white text-white">
-            <ion-icon clase="w-5 h-5" name="document-text-outline"></ion-icon>
+        <span class="absolute flex items-center justify-center w-14 h-14 bg-[#68a3dd] rounded-full -left-3 ring-8 ring-white text-white">
+            <ion-icon class="w-8 h-8" name="document-text-outline"></ion-icon>
         </span>
             <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Cumplimenta</h3>
             <p class="text-base font-normal text-gray-500 dark:text-gray-400">Introduce los datos personales que indica la pasarela de pago y haz click.</p>
         </li>
         <li class="ml-6">
-        <span class="absolute flex items-center justify-center w-8 h-8 bg-[#4f68c6] rounded-full -left-3 ring-8 ring-white text-white">
-            <img clase="w-5 h-5" src="https://www.helloflatmate.com/img/img/icon-calendar-check.png">
+        <span class="absolute flex items-center justify-center w-14 h-14 bg-[#4f68c6] rounded-full -left-3 ring-8 ring-white text-white">
+            <img class="w-8 h-8" src="https://www.helloflatmate.com/img/img/icon-calendar-check.png">
         </span>
             <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Reserva sin esperas</h3>
             <p class="text-base font-normal text-gray-500 dark:text-gray-400">Trabajamos con el Cyberpac más seguro del mercado español (La Caixa) Cumplimenta los datos que te pide, una vez hecho y realizado el pago recibirás un correo electrónico de confirmación. ¡Y ya está!, habitación reservada sin esperas de confirmación.</p>
         </li>
         <li class="ml-6">
-        <span class="absolute flex items-center justify-center w-8 h-8 bg-[#3f16a5] rounded-full -left-3 ring-8 text-white ring-white">
-            <ion-icon clase="w-5 h-5" name="mail-outline"></ion-icon>
+        <span class="absolute flex items-center justify-center w-14 h-14 bg-[#3f16a5] rounded-full -left-3 ring-8 text-white ring-white">
+            <ion-icon class="w-8 h-8" name="mail-outline"></ion-icon>
         </span>
             <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Confirmación</h3>
             <p class="text-base font-normal text-gray-500 dark:text-gray-400">En breve recibirás un correo electrónico para organizar tu llegada a Valencia. Y así tener tu contrato y llaves preparadas. ¡Bienvenida/o a Valencia! ¡Bienvenida/o a helloflatmate!</p>
