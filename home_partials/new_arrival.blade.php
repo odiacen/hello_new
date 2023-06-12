bf4680d72ace3bdc206998695d3b207f290e32cf
CARLOS <gandalf930526@gmail.com>
2023-06-12 14:28:44 -0400

    arreglando arrival

--- home_partials/new_arrival.blade.php
+++ home_partials/new_arrival.blade.php
@@ -8,18 +8,22 @@
         -webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 30% 50%);
         clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%, 30% 50%);
     ">
-        <button onclick="playVideo()" class="play-button absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-900 text-white px-6 py-3 rounded-full"><i class="fas fa-play"></i></button>
+        <div class="play-button absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-900 text-white px-6 py-3 rounded-full">
+            <i class="fas fa-play" onclick="playVideo()"></i>
+        </div>
     </div>
 </div>
 
 <script>
     function playVideo() {
-        var videoUrl = 'https://www.youtube.com/watch?v=3KSXxo8CbpY ';
+        var videoUrl = 'https://www.youtube.com/watch?v=nyxTSCVz8w8';
         var embedUrl = videoUrl.replace('watch?v=', 'embed/');
         var videoContainer = document.querySelector('.video-container');
         var textContainer = document.getElementById('text-container');
+        var playButton = document.querySelector('.play-button');
 
         textContainer.style.display = 'none'; // Oculta el contenedor de texto
+        playButton.style.display = 'none'; // Oculta el botón de reproducción
 
         videoContainer.innerHTML = '<iframe width="100%" height="100%" src="' + embedUrl + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         videoContainer.style.width = '100%';
