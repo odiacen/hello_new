49d1805480c2ce525251a81b79097783f6926c90
odiacen <96943520+odiacen@users.noreply.github.com>
2023-06-13 07:27:44 -0400

    add new alpine

--- new_room.blade.php
+++ new_room.blade.php
@@ -6,6 +6,7 @@
 @include('nuevo.room_partials.new_district')
 @include('nuevo.room_partials.new_other_rooms')
 @include('nuevo.room_partials.new_reservations')
+@include('nuevo.room_partials.new_alpine_room')
 </section>
 @include('nuevo.global.new_faqs')
 @include('nuevo.global.new_footer')
